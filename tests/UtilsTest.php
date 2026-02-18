0e5c7f19f5f6052793df44e94b629a8040d8311a
Evgenii Ivanov <ievgesha@mail.ru>
2026-02-18 13:25:42 +0700

    Add: BigText Test

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -5,12 +5,18 @@ namespace Hexlet\Phpunit\Tests;
 use PHPUnit\Framework\TestCase;
 
 use function Hexlet\Phpunit\Utils\reverseString;
+use function Hexlet\Phpunit\Tests\Utils\getFixturesFullPath;
 
 class UtilsTest extends TestCase
 {
     public function testReverse(): void
     {
         $this->assertEquals('', reverseString(''));
+
         $this->assertEquals('olleh', reverseString('hello'));
+
+        $bigText = file_get_contents(getFixturesFullPath('BigText.txt'));
+        $reverseBigText = file_get_contents(getFixturesFullPath('ReverseBigText.txt'));
+        $this->assertEquals($reverseBigText, reverseString($bigText));
     }
 }
